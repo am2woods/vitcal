07aee322573ea5e97f5db3f8e3ffcff7d42fea0e
Omar Otieku <ootieku@gmail.com>
2016-02-06 21:04:48 -0500

    corrections

--- vitamins.php
+++ vitamins.php
@@ -13,17 +13,17 @@ class Vitamins {
         return $this->db->query('SELECT * FROM vitamins');
     }
 
-    public function getData($_GET['name']) {
-  		$name = $db->quote($_GET['name']);
-  		$sql = "
-			SELECT 'v'.'name', 'f'.'name', 'p'.'grams', 
-					'p'.'percentage', 'i'.'path'
-			FROM 'vitamins' 'v' JOIN 'percentage' 'p' ON 'v'.'id' = 'p'.'vitamin_id'
-								JOIN 'food' 'f' ON 'f'.'id' = 'p'.'food_id'
-								JOIN 'images' 'i' ON 'i'.'id' = 'f'.'image_id'
-			WHERE 'v'.'name' = '". $name ."'
+	/*
+	 * This method will run the query when called
+	 */
+    public function getData($name) {
+  		$nameEscaped = $db->quote($name);
+  		$sql = "SELECT 'v'.'name', 'f'.'name', 'p'.'grams', 'p'.'percentage', 'i'.'path' " .
+					"FROM 'vitamins' 'v' JOIN 'percentage' 'p' ON 'v'.'id' = 'p'.'vitamin_id' " .
+						"JOIN 'food' 'f' ON 'f'.'id' = 'p'.'food_id' " .
+						"JOIN 'images' 'i' ON 'i'.'id' = 'f'.'image_id' " .
+					"WHERE 'v'.'name' = {$nameEscaped}";
 
-		";
   		return $this->db->query($sql);
 	}
 }
--- vitamins/data.php
+++ vitamins/data.php
@@ -1,12 +1,40 @@
 <?php
 
-if(isset($_GET['name'])) {
+if(!isset($_GET['name'])) die('A vitamin name is required.');
+
 	require '../vitamins.php';
 
-	getData($data);
+	/*
+	 * Make the connection to the database
+	 */
+	$pdo = new PDO('mysql:host=localhost;dbname=vitamins', 'root', 'root');
 
-	echo $data['
-		//markup
-	']
+	/*
+	 * Instantiate a new Vitamins instance
+	 */
+	$table = new Vitamins($pdo);
 
-}
+	//get the name from the query parameter
+	$param = $_GET['name'];
+
+	/*
+	 * Call our method which containers the query
+	 */
+	$data = $table->getData($param);
+
+
+	/*
+	 * Loop through the data and output your markup
+	 */
+?>
+<div>
+	<?php while($data): ?>
+		<h3><?php echo $data['name']; ?></h3>
+	<?php endWhile; ?>
+</div>
+
+
+
+
+
+

dae3325ad2f10852b98fb4febd94e0f49ab33393
am2woods <am2woods@gmail.com>
2016-02-06 20:18:03 -0500

    data.php and vitamins.php new update

--- vitamins.php
+++ vitamins.php
@@ -12,5 +12,19 @@ class Vitamins {
     {
         return $this->db->query('SELECT * FROM vitamins');
     }
+
+    public function getData($_GET['name']) {
+  		$name = $db->quote($_GET['name']);
+  		$sql = "
+			SELECT 'v'.'name', 'f'.'name', 'p'.'grams', 
+					'p'.'percentage', 'i'.'path'
+			FROM 'vitamins' 'v' JOIN 'percentage' 'p' ON 'v'.'id' = 'p'.'vitamin_id'
+								JOIN 'food' 'f' ON 'f'.'id' = 'p'.'food_id'
+								JOIN 'images' 'i' ON 'i'.'id' = 'f'.'image_id'
+			WHERE 'v'.'name' = '". $name ."'
+
+		";
+  		return $this->db->query($sql);
+	}
 }
 
--- vitamins/data.php
+++ vitamins/data.php
@@ -1,21 +1,12 @@
 <?php
 
 if(isset($_GET['name'])) {
-	require '../db/vitamins.php';
+	require '../vitamins.php';
 
-	$name =  ($_GET['name']);
+	getData($data);
 
-	$data = mysql_query("
-			SELECT 'v'.'name', 'f'.'name', 'p'.'grams', 
-					'p'.'percentage', 'i'.'path'
-			FROM 'vitamins' 'v' JOIN 'percentage' 'p' ON 'v'.'id' = 'p'.'vitamin_id'
-								JOIN 'food' 'f' ON 'f'.'id' = 'p'.'food_id'
-								JOIN 'images' 'i' ON 'i'.'id' = 'f'.'image_id'
-			WHERE 'v'.'name' = '". $name ."'
+	echo $data['
+		//markup
+	']
 
-		");
 }
-
-echo $data['
-
-']
